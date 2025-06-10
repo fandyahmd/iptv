ea20058ad9cd71ed917b0fcc3a4c8d97404630ce
fandyahmd <fandyahmadazizi@gmail.com>
2025-06-10 10:19:13 +0700

    Refactor merge process to improve clarity and update success message in merge.py

--- merge.py
+++ merge.py
@@ -1,6 +1,6 @@
 import json
 
-with open('1.json', 'r') as f1, open('2.json', 'r') as f2, open('add.json', 'r') as f3:
+with open('1.json', 'r') as f1, open('add.json', 'r') as f2, open('2.json', 'r') as f3:
     data1 = json.load(f1)
     data2 = json.load(f2)
     data3 = json.load(f3)
@@ -10,4 +10,4 @@ combined = data1 + data2 + data3
 with open('id.json', 'w') as f:
     json.dump(combined, f, indent=2)
 
-print("✅ id.json berhasil dibuat dengan gabungan dari 1.json, 2.json, add.json")
+print("✅ id.json created!")
